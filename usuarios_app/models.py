695c069685f86483571f94389a774c69911fa110
Isabelle <isabellepontes91@gmail.com>
2023-10-11 04:37:21 -0300

    templates, modelos, views

--- usuarios_app/models.py
+++ usuarios_app/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Usuario:
-    nome = models.TextField(min_legth = 1)
-    sobrenome = models.TextField(min_legth = 1)
+    nome = models.TextField()
+    sobrenome = models.TextField()
     email = models.EmailField(max_length = 100)
     telefone = models.CharField(max_length = 11)
 
