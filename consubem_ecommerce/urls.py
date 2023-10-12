f67f0e82bc95579da2e9e734a8a6fbdb2e73ded5
lucasfelix1803 <lucasfelix210917@gmail.com>
2023-10-12 17:55:56 -0300

    url's do projeto e do app

--- consubem_ecommerce/urls.py
+++ consubem_ecommerce/urls.py
@@ -13,15 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#from django.contrib import admin
-from django.urls import path
-from usuarios_app import views
+from django.contrib import admin
 from django.urls import path, include
-
+from . import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path("", views.index ),
-    
+    path('clientes/', include ('usuarios_app')),
+    path('admin/', admin.site.urls),
 ]
